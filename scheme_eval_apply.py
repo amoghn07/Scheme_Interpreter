df98610d384b06911870e4e9288b5bc3899b8cb9
thaliagnuyen <thaliangu283@gmail.com>
2026-05-15 10:14:48 -0700

    Problem 3
    Implement evaluation and application of Scheme expressions.

--- scheme_eval_apply.py
+++ scheme_eval_apply.py
@@ -35,7 +35,9 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
         return SPECIAL_FORMS[first](rest, env)
     else:
         # BEGIN PROBLEM 3
-        "*** YOUR CODE HERE ***"
+        operator = scheme_eval(first, env)
+        operands = rest.map(lambda x: scheme_eval(x, env))
+        return scheme_apply(operator, operands, env)
         # END PROBLEM 3
 
 def scheme_apply(procedure, args, env):

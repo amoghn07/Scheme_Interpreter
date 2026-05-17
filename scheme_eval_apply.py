e51ea218d464fdb5ef65e6a5fc9fe1f1f302fcb7
Thomas K <zjthomaskuo@gmail.com>
2026-05-16 19:04:02 -0700

    Complete problems 1 and 2

--- scheme_eval_apply.py
+++ scheme_eval_apply.py
@@ -48,11 +48,17 @@ def scheme_apply(procedure, args, env):
        assert False, "Not a Frame: {}".format(env)
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
-        "*** YOUR CODE HERE ***"
+        py_args = []
+        link = args
+        while link is not Link.empty:
+            py_args.append(link.first)
+            link = link.rest
+        if procedure.need_env:
+            py_args.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
-            "*** YOUR CODE HERE ***"
+            return procedure.py_func(*py_args)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
